74bd8da5c337217396a3422d53f94825e7eacc09
Victor Gomes <victorgvgomes@gmail.com>
2026-09-04 08:04:30 -0300

    Reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,6 +14,7 @@ public class Program {
 		Department obj = new Department (1, "Books");
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById ====");
 		
 		Seller seller = sellerDao.findbyId(3);
 		System.out.println(seller);
