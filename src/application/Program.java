ca766c785ae690c9e05b019e8852a130bd307597
Victor Gomes <victorgvgomes@gmail.com>
2026-09-07 15:38:47 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -41,6 +41,17 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: seller Update ====");
+		
+		seller = sellerDao.findbyId(1);
+		seller.setName("Martha Wayne");
+		seller.setEmail("MartaWayne@gmail.com");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
+		System.out.println(sellerDao.findbyId(1));
+		
+
+		
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -38,6 +38,7 @@ public class SellerDaoJDBC implements SellerDao {
 			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
 			st.setDouble(4, obj.getBaseSalary());
 			st.setInt(5, obj.getDepartment().getId());
+			
 			int rowsAffected = st.executeUpdate();
 			if (rowsAffected>0) {
 				ResultSet rs = st.getGeneratedKeys();
@@ -67,7 +68,30 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		
+	
+		
+		PreparedStatement st = null; 
+		try {
+			st = conn.prepareStatement("UPDATE seller SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? WHERE Id = ? ");
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			st.executeUpdate();	
+			
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+			
+		}
+		finally {
+			
+			DB.closeStatement(st);
+		
+		}
 		
 	}
 
